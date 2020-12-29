52b864928919cf387d3f1b089a09a0a5c3fcb433
John McDonald <jmcdonald1991@gmail.com>
2020-12-28 23:23:26 -0600

    closest first method, spawn

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -13,6 +13,7 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SetTargetEnemy();
         if(targetEnemy)
         {
             objectToPan.LookAt(targetEnemy);
@@ -22,6 +23,29 @@ public class Tower : MonoBehaviour
             Shoot(false);
         }
         
+    }
+
+    private void SetTargetEnemy()
+    {
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+        if(sceneEnemies.Length == 0) { return; }
+        Transform closestEnemy = sceneEnemies[0].transform;
+        foreach(EnemyDamage testEnemy in sceneEnemies)
+        {
+            closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+        }
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform GetClosest(Transform transformA, Transform transformB)
+    {
+        var distToA = Vector3.Distance(transform.position, transformA.position);
+        var distToB = Vector3.Distance(transform.position, transformB.position);
+        if(distToA < distToB)
+        {
+            return transformA;
+        }
+        return transformB;
     }
 
     private void FireAtEnemy()
