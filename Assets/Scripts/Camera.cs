229fed7d274741c8e47d874c761d1ffcc79a7b3a
John McDonald <jmcdonald1991@gmail.com>
2020-12-29 18:20:27 -0600

    title screen menu and game music

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Camera : MonoBehaviour
+{
+    public Button startButton;
+
+    void Start()
+    {
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        LoadScene();
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.down, 5.0f * Time.deltaTime);
+        transform.Rotate(Vector3.forward, 5.0f * Time.deltaTime);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
